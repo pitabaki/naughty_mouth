b8fe35d3fb7f48eb3e83edc4c429a3118aea63ba
Peter Berki <pberki1986@gmail.com>
2020-01-20 16:51:40 -0800

    Init. Res.render issue present

---
+++ server/config/headers.js
@@ -0,0 +1,12 @@
+const httpHeaders = {
+    'X-Frame-Options' : 'allow-from https://kumadev.com/',
+    'X-Xss-Protection' : '1; mode=block' ,
+    'X-Content-Type-Options' : 'nosniff' ,
+    'Content-Security-Policy' : 'upgrade-insecure-requests' ,
+    'Strict-Transport-Security' : 'max-age=31536000' ,
+    'Referrer-Policy' : 'strict-origin-when-cross-origin',
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Credentials' : 'true'
+}
+
+module.exports = httpHeaders;
---
+++ server/config/index.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  development: {
+    sitename: 'Naughty Mouth [Development]',
+  },
+  production: {
+    sitename: 'Naughty Mouth',
+  },
+}
